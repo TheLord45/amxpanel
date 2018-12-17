952e8c8557b6df85b383b276239e6380442fae44
andreas <andreas@27087943-deb9-4f23-b7a5-b6ab4475e261>
2018-12-17 15:52:09 +0000

    more code

---
+++ src/nameformat.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 by Andreas Theofilu <andreas@theosys.at>
+ *
+ * All rights reserved. No warranty, explicit or implicit, provided.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Andreas Theofilu and his suppliers, if any.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Andreas Theofilu and its suppliers and
+ * may be covered by European and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Andreas Theofilu.
+ */
+
+#ifndef __NAMEFORMAT_H__
+#define __NAMEFORMAT_H__
+
+#include "strings.h"
+
+class NameFormat
+{
+	public:
+		NameFormat();
+		~NameFormat();
+
+		static strings::String toValidName(strings::String& str);
+		static strings::String cutNumbers(strings::String& str);
+};
+
+#endif
