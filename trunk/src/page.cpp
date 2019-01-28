b7a943bf75df0a99ee4bdeb1911a0f35b37ae19a
andreas <andreas@27087943-deb9-4f23-b7a5-b6ab4475e261>
2019-01-28 17:08:38 +0000

    JS erweitert.

--- trunk/src/page.cpp
+++ trunk/src/page.cpp
@@ -466,13 +466,14 @@ void amx::Page::generateButtons()
 		for (size_t i = 0; i < page.buttons.size(); i++)
 		{
 			if (page.buttons[i].ap > 0 || page.buttons[i].ad > 0 ||
-				page.buttons[i].cp > 0 || page.buttons[i].ch > 0)
+				page.buttons[i].cp > 0)
 			{
 				if (!btArray.empty())
 					btArray += ",";
 
-				btArray += String("\"Page")+page.pageID+"_b"+i+"_Button_"+page.buttons[i].bi+"\":";
-				btArray += String("{\"ap\":")+page.buttons[i].ap+",\"ac\":"+page.buttons[i].ad;
+				btArray += String("{\"pnum\":")+page.pageID+",\"bi\":"+page.buttons[i].bi+",";
+				btArray += String("\"instances\":")+page.buttons[i].sr.size()+",";
+				btArray += String("\"ap\":")+page.buttons[i].ap+",\"ac\":"+page.buttons[i].ad;
 				btArray += String(",\"cp\":")+page.buttons[i].cp+",\"ch\":"+page.buttons[i].ch+"}";
 			}
 
