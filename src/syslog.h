7db7f6130e6cb97adae86ecff98f5c34d5020e72
Andreas Theofilu <andreas@theosys.at>
2019-07-06 10:57:43 +0200

    Changed license to GPLv3

--- src/syslog.h
+++ src/syslog.h
@@ -1,17 +1,19 @@
 /*
  * Copyright (C) 2015 by Andreas Theofilu <andreas@theosys.at>
  *
- * All rights reserved. No warranty, explicit or implicit, provided.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- * NOTICE:  All information contained herein is, and remains
- * the property of Andreas Theofilu and his suppliers, if any.
- * The intellectual and technical concepts contained
- * herein are proprietary to Andreas Theofilu and its suppliers and
- * may be covered by European and Foreign Patents, patents in process,
- * and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Andreas Theofilu.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 #ifndef __SYSLOG_H_
